2b1d6cd258944d20a48f9d004a22e0597eaecec7
Patrick Chan <patrickchan1999@gmail.com>
2018-07-17 22:14:36 -0400

    Extract directory search to a separate function

--- src/files_searcher.rs
+++ src/files_searcher.rs
@@ -7,23 +7,25 @@ use Search;
 
 pub struct FilesSearcher;
 
+fn search_directory(root_directory: String) -> Vec<String> {
+    let mut candidates = Vec::new();
+    for entry in WalkDir::new(root_directory).contents_first(true).into_iter().filter_map(|e| e.ok()) {
+        if entry.file_type().is_dir() {
+            continue;
+        }
+        candidates.push(entry.path().to_string_lossy().into_owned());
+    }
+    candidates
+}
+
 impl Search for FilesSearcher {
 
     fn search() -> Vec<String> {
-        // the following line is used during testing
-        // let root_path = format!("{}/src", env!("CARGO_MANIFEST_DIR"));
         let root_path = match dirs::home_dir() {
             Some(path) => path.to_string_lossy().into_owned(),
             None => String::from("")
         };
-        let mut candidates = Vec::new();
-        for entry in WalkDir::new(root_path).contents_first(true).into_iter().filter_map(|e| e.ok()) {
-            if entry.file_type().is_dir() {
-                continue;
-            }
-            candidates.push(entry.path().to_string_lossy().into_owned());
-        }
-        candidates
+        search_directory(root_path)
     }
     
 }
@@ -31,14 +33,14 @@ impl Search for FilesSearcher {
 #[cfg(test)]
 mod tests {
 
-    use Search;
-    use files_searcher::FilesSearcher;
+    use files_searcher::search_directory;
 
     #[test]
     fn simple_search() {
+        let root_path = format!("{}/src", env!("CARGO_MANIFEST_DIR"));
         let actual_candidates = ["lib.rs", "files_searcher.rs", "words_alpha.txt",
                                  "app_searcher.rs", "wordlist_searcher.rs"];
-        let candidates = FilesSearcher::search();
+        let candidates = search_directory(root_path);
         for i in 0..candidates.len() {
             println!("{}", candidates[i]);
             assert!(candidates[i].ends_with(actual_candidates[i]));

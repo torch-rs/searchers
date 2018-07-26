db39b79dc665fc28d1de5852903fad47afffa805
Patrick Chan <patrickchan1999@gmail.com>
2018-07-26 16:39:26 -0400

    Change Vec<String> to Vec<SearchCandidate> in preparation for icons

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
+extern crate search_candidate;
+
 pub mod app_searcher;
 pub mod files_searcher;
 pub mod windows_searcher;
 pub mod wordlist_searcher;
 
+use self::search_candidate::SearchCandidate;
+
 pub trait Search {
 
-    fn search() -> Vec<String>;
+    fn search() -> Vec<SearchCandidate>;
 
 }
